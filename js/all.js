2b7c43e18f42d060662e269e52f1832e981cf845
Chaoci <charlieischao@gmail.com>
2022-07-08 10:53:21 +0800

    ya

--- js/all.js
+++ js/all.js
@@ -9,30 +9,30 @@ const deluxe_twin = document.querySelector('.d-twin');
 
 single.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_002.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_0126.png.png'>";
 });
 
 deluxe_single.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_0123.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_002.png.png'>";
 });
 
 double.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_0124.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_0123.png.png'>";
 });
 
 deluxe_double.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_0125.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_0124.png.png'>";
 });
 
 twin.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_0126.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_0125.png.png'>";
 });
 
 deluxe_twin.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    picture.innerHTML="<img src='https://github.com/Chaoci/Timer/blob/main/img_deluxesingle_002.png.png?raw=true'>";
+    picture.innerHTML="<img src='image/img_deluxesingle_0123.png.png'>";
 });
